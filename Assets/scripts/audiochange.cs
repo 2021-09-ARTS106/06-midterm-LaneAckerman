fef23fadd46711ca5cd6a7b02d75ab1f2f4f420b
LaneAckerman <71029213+LaneAckerman@users.noreply.github.com>
2021-11-03 12:53:21 -0400

    audio

--- Assets/scripts/audiochange.cs
+++ Assets/scripts/audiochange.cs
@@ -4,56 +4,20 @@ using UnityEngine;
 
 public class audiochange : MonoBehaviour
 {
+    //public AudioClip tension;
+    public AudioSource tensionZone;
+    public AudioSource background;
+
     // Start is called before the first frame update
-    public static SoundEffectsHelper Instance;
-
-    public AudioClip explosionSound;
-    public AudioClip playerShotSound;
-    public AudioClip enemyShotSound;
-
-    void Awake()
-    {
-        // Register the singleton
-        if (Instance != null)
-        {
-            Debug.LogError("Multiple instances of SoundEffectsHelper!");
-        }
-        Instance = this;
-    }
-
-    public void MakeExplosionSound()
-    {
-        MakeSound(explosionSound);
-    }
-
-    public void MakePlayerShotSound()
-    {
-        MakeSound(playerShotSound);
-    }
-
-    public void MakeEnemyShotSound()
-    {
-        MakeSound(enemyShotSound);
-    }
-
-    /// <summary>
-    /// Play a given sound
-    /// </summary>
-    /// <param name="originalClip"></param>
-    private void MakeSound(AudioClip originalClip)
-    {
-        // As it is not 3D audio clip, position doesn't matter.
-        AudioSource.PlayClipAtPoint(originalClip, transform.position);
-    }
-}
-
-    void OnTriggerEnter(other : Collider)
+    
+    void OnTriggerEnter(Collider other)
         {
         // Make sure your character is tagged 'player'
-            if (other.gameObject.tag == "player")
+            if (other.CompareTag("player"))
             {
-            // Plays the audio that you want...
-                audio.Play();
+        // Plays the audio that you want...
+                background.Stop();
+                tensionZone.Play();
             }
-    }
+        }
 }
