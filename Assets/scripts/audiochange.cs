44eacdf5fd67ebca9e7c84622c352d3ac5eb49c1
LaneAckerman <71029213+LaneAckerman@users.noreply.github.com>
2021-12-01 12:21:30 -0500

    Update audiochange.cs

--- Assets/scripts/audiochange.cs
+++ Assets/scripts/audiochange.cs
@@ -7,17 +7,19 @@ public class audiochange : MonoBehaviour
     //public AudioClip tension;
     public AudioSource tensionZone;
     public AudioSource background;
+    private const string Message = "hey!";
 
     // Start is called before the first frame update
-    
+
     void OnTriggerEnter(Collider other)
         {
         // Make sure your character is tagged 'player'
-        if (other.gameObject.CompareTag("player"))
+        if (other.gameObject.CompareTag("Player"))
             {
         // Plays the audio that you want...
                 background.Stop();
                 tensionZone.Play();
-            }
+                Debug.Log(Message);
+        }
         }
 }
